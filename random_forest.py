18048d16344fe6254c0c08d68350c254d7fa9d92
sanket1sinha <31497077+sanket1sinha@users.noreply.github.com>
2018-04-27 15:45:50 -0500

    Update
    Added the function randomForest

--- random_forest.py
+++ random_forest.py
@@ -1,3 +1,16 @@
 class RandomForest:
-    #TODO sanket
-    pass
+    def randomForest(train_data,test_data):
+        tree_count = 180
+        bag_proportion = .90
+        result=[]
+        for i in range(tree_count):
+            bag = insurance_training.sample(frac=bag_proportion, replace=True, random_state=i)
+            clf = tree.DecisionTreeClassifier(random_state=1, min_samples_leaf=2)
+            clf.fit(bag[cols], bag["Response"])
+            insurance_testing_result = clf.predict(insurance_testing)
+            result.append(insurance_testing_result)
+        df=pd.DataFrame(result)
+        m=df.mode()
+        final=m.iloc[0].values.tolist()
+        final=[int(i) for i in final]
+        return final
