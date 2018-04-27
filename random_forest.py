cbf14e3bcd1754bf452626d72c7ab7d4bbd78924
sanket1sinha <31497077+sanket1sinha@users.noreply.github.com>
2018-04-27 15:54:39 -0500

    Update random_forest.py

--- random_forest.py
+++ random_forest.py
@@ -4,10 +4,10 @@ class RandomForest:
         bag_proportion = .90
         result=[]
         for i in range(tree_count):
-            bag = insurance_training.sample(frac=bag_proportion, replace=True, random_state=i)
+            bag = train_data.sample(frac=bag_proportion, replace=True, random_state=i)
             clf = tree.DecisionTreeClassifier(random_state=1, min_samples_leaf=2)
             clf.fit(bag[cols], bag["Response"])
-            insurance_testing_result = clf.predict(insurance_testing)
+            insurance_testing_result = clf.predict(test_data)
             result.append(insurance_testing_result)
         df=pd.DataFrame(result)
         m=df.mode()
