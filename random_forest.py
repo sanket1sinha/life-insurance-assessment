3f7c98b44867ca007cb3f26265f2f4de406a08e0
sanket1sinha <31497077+sanket1sinha@users.noreply.github.com>
2018-04-29 12:11:06 -0500

    update

--- random_forest.py
+++ random_forest.py
@@ -1,4 +1,9 @@
 class RandomForest:
+    train_data = pd.read_csv("training.csv")
+    test_data = pd.read_csv("testing.csv")
+    train_data=train_data.fillna(train_data.mean())
+    test_data=test_data.fillna(test_data.mean())
+    cols=test_data.columns.tolist()
     def randomForest(train_data,test_data):
         tree_count = 180
         bag_proportion = .90
